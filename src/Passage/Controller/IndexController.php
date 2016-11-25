7e22e8c9e794c9b9c2d1aa7b9c49d9fbb00ddd4a
vincent mazet <vincentmazet42@gmail.com>
2016-11-25 17:09:47 +0100

    added simple controller

--- src/Passage/Controller/IndexController.php
+++ src/Passage/Controller/IndexController.php
@@ -7,5 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 class IndexController
 {
+	public function getLine(Request $request, Application $app)
+	    {
+		$parameters = $request->request->all();
+       		 $idLine = $parameters['id_line'];
+ 		 $result = $app['repository.passage']->getLine($idLine);
+
+		return $result;
+	    }
 
 }
