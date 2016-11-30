4b6184fac66ceb08c6926421e653d04270ec232f
VERCRUYSSE ALEXY p1615306 <alexy.vercruysse@etu.univ-lyon1.fr>
2016-11-30 10:32:59 +0100

    Complete stop object repo and controller

---
+++ src/Stop/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Stop\Controller;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController
+{
+
+
+
+
+
+}
---
+++ src/Stop/Repository/StopRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Stop\Repository;
+
+use App\Stop\Entity\Stop;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * Passage repository.
+ */
+class StopRepository
+{
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+
+
+}
